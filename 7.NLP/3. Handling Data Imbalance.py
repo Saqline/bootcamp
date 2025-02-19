bb8957a2c1c4787ce7ee39701799cc0e2aab0818
Imran Nur Tanha <82803676+Saqline@users.noreply.github.com>
2025-02-19 17:35:21 +0000

    update code

--- 7.NLP/3. Handling Data Imbalance.py
+++
@@ -1,7 +0,0 @@
-from imblearn.over_sampling import SMOTE
-from sklearn.model_selection import train_test_split
-
-X_train, X_test, y_train, y_test = train_test_split(X_tfidf, [1, 0], test_size=0.2, random_state=42)
-
-smote = SMOTE(random_state=42)
-X_train_resampled, y_train_resampled = smote.fit_resample(X_train, y_train)
